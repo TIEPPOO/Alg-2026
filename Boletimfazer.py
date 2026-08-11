3275d56dde8df9a267ce9f3a6dc46b5abb05fa0b
Enderson TIEPPO <endersontieppo@gmail.com>
2026-08-11 20:25:39 -0300

    Create Boletimfazer.py for student grade calculation

---
+++ Boletimfazer.py
@@ -0,0 +1,16 @@
+print ("Boletim escolar")
+
+Nome = input ("Nome")
+Curso = input ("Curso")
+Semestre = input ("Semestre")
+Disciplina = input ("Disciplina")
+nota1 = float (input(nota 1))
+nota2 = float (input(nota 2))
+
+media = (nota1 + nota2) / 2
+
+if media >=100
+print ("Erro de Lançamento")
+
+elif media <19
+print ("Reprovado")
